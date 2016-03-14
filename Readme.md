f765d9c24577b2cdd7578275f0fc3e5da9517983
Parham Negahdar <pnegahdar@gmail.com>
2016-03-14 03:42:14 -0400

    First commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+## cronli: a cli cron tool
+
+Lets you run crons with second granularity as a standard cli proc
+
+Very light wrapper around: https://github.com/robfig/cron
+
+Installation:
+
+Grab the right precompiled bin from github releases and put it in your path. Don't forget to `chmod +x` the bin.
+
+For example:
+
+    curl -SL https://github.com/pnegahdar/cronli/releases/download/0.1.0/cronli_0.1.0_darwin_amd64.tar.gz \
+        | tar -xzC /usr/local/bin --strip 1 && chmod +x /usr/local/bin/cronli
+
+Usage:
+
+    cronli "* * * * *" "echo hello"
+
+Arg 1: Schedule see: [schedule format](https://godoc.org/github.com/robfig/cron#hdr-CRON_Expression_Format)
+
+Arg 2: The command you would put into /bin/bash -c "THIS COMMAND"
---
+++ src/cronli/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/robfig/cron"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+	"fmt"
+)
+
+func main() {
+	c := cron.New()
+	if len(os.Args) != 3 {
+		println("Please provide 2 args: schedule and bash command")
+		os.Exit(1)
+	}
+	schedule := os.Args[1]
+	command := []string{"/usr/bin/env", "sh", "-c", os.Args[2]}
+	c.AddFunc(schedule, func() {
+		cmd := exec.Command(command[0], command[1:]...)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	})
+	fmt.Println("Schedule:", schedule)
+	fmt.Println("Command:", strings.Join(command, " "))
+	firstRuns := []string{}
+	next := time.Now().Local()
+	for i := 0; i < 6; i++ {
+		next = c.Entries()[0].Schedule.Next(next)
+		firstRuns = append(firstRuns, next.String())
+	}
+	fmt.Println("First Runs:", strings.Join(firstRuns, ", "))
+	fmt.Println("Started.")
+	fmt.Println("********")
+	c.Start()
+	for {
+	}
+}
